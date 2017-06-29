c9f0bce0279cf368824d5dbc2e179a5c51ba419b
rudrasohan <sohanrudra@gmail.com>
2017-06-29 17:34:13 +0530

    Insert & Delete implemented

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -11,14 +11,20 @@ Node* head = NULL;
 
 void create_list();
 void display();
-//void insert_at_beginning(int x);
-//void insert_(int x,int n);
-//void delete_(int x,int n);
+void insert_at_beginning(int);
+void insert_(int ,int );
+void delete_(int);
 
 
 int main()
 {
 	create_list();
+	display();
+	insert_at_beginning(24);
+	display();
+	insert_(25,2);
+	display();
+	delete_(2);
 	display();
 	return 0;
 }
@@ -51,15 +57,56 @@ void create_list()
 		}
 	}
 
+
 }
 
 void display()
 {
-	cout <<"Test"<<endl;
+	//cout <<"Test"<<endl;
 	Node *temp = head;
 	while(temp != NULL)
 	{
 		cout << temp->data << endl;
 		temp = temp->next;
 	}
+}
+
+void insert_at_beginning(int x)
+{
+	Node *temp = new Node();
+	temp->data = x;
+	temp->next = NULL;
+	Node *ptr = head;
+	head = temp;
+	temp->next = ptr;
+}
+
+void insert_(int x, int p)
+{
+	Node *temp = head;
+	int c = 1;
+	while((temp != NULL)&&(c != p))
+	{
+		temp = temp->next;
+		c++;
+	}
+	Node* temp1 = new Node();
+	temp1->data = x;
+	temp1->next = NULL;
+	temp1->next = temp->next;
+	temp->next = temp1;		
+}
+
+void delete_(int p)
+{
+	Node *temp = head;
+	int c = 1;
+	while((temp != NULL)&&(c != p))
+	{
+		temp = temp->next;
+		c++;
+	}
+	Node *ptr = temp->next;
+	temp->next = ptr->next;
+	delete ptr;
 }
